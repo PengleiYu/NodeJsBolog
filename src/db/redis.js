43878d793c62d19230467b3451ae1f047a29d5d1
penglei <yupengniaosanshi@gmail.com>
2019-08-29 00:07:25 +0800

    封装redis操作

---
+++ src/db/redis.js
@@ -0,0 +1,35 @@
+const redis = require('redis');
+const { REDIS_CONF } = require('../config/db')
+
+const redisClient = redis.createClient(REDIS_CONF.port, REDIS_CONF.host)
+redisClient.on('error', err => {
+    console.error(err)
+})
+function set(key, val) {
+    if (typeof val == 'object') {
+        val = JSON.stringify(val)
+    }
+    redisClient.set(key, val, redis.print)
+}
+function get(key) {
+    const promise = new Promise((resolve, reject) => {
+        redisClient.get(key, (err, val) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            console.log('val ', val)
+            if (val == null) resolve(null)
+            try {
+                resolve(JSON.parse(val))
+            } catch (e) {
+                resolve(val)
+            }
+        })
+    })
+    return promise
+}
+module.exports = {
+    set,
+    get
+}
