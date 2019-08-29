bb588a17a5043b8f7b65a4f00461c148679b4b7f
penglei <yupengniaosanshi@gmail.com>
2019-08-29 10:06:01 +0800

    添加登录拦截检查

--- src/router/blog.js
+++ src/router/blog.js
@@ -4,6 +4,14 @@ const { getList,
     updateBlog,
     delBlog } = require('../controller/blog')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
+
+// 登录检查
+const checkLogin = (req) => {
+    if (!req.session.username) {
+        return Promise.resolve(new ErrorModel('尚未登录'))
+    }
+}
+
 const handleBlogRouter = async (req, res) => {
     const method = req.method
     const id = req.query.id;
@@ -19,17 +27,21 @@ const handleBlogRouter = async (req, res) => {
         if (req.path === '/api/blog/detail') {
             const detail = await getDetail(id)
             return new SuccessModel(detail)
-            // return { msg: '这里是博客详情接口' }
         }
     }
+
+    // 进行登录拦截
+    const checkLoginResult = await checkLogin(req)
+    if (checkLoginResult) {
+        return checkLoginResult
+    }
+
     if (method === 'POST') {
         // blog新建
         if (req.path === '/api/blog/new') {
-            const author = 'zhangsan'
-            req.body.author = author
+            req.body.author = req.session.username
             const data = await newBlog(req.body)
             return new SuccessModel(data)
-            // return { msg: '这里是新建博客接口' }
         }
         // blog更新
         if (req.path === '/api/blog/update') {
@@ -39,18 +51,15 @@ const handleBlogRouter = async (req, res) => {
             } else {
                 return new ErrorModel('更新博客失败')
             }
-            // return { msg: '这里是更新博客接口' }
         }
         // blog删除
         if (req.path === '/api/blog/del') {
-            const author = 'zhangsan'
-            const result = await delBlog(id, author)
+            const result = await delBlog(id, req.session.username)
             if (result) {
                 return new SuccessModel()
             } else {
                 return new ErrorModel('删除博客失败')
             }
-            // return { msg: '这里是删除博客接口' }
         }
     }
 }
--- src/router/user.js
+++ src/router/user.js
@@ -4,11 +4,10 @@ const { get, set } = require('../db/redis')
 
 const handleUserRouter = async (req) => {
     const method = req.method
-    if (method === 'GET') {
+    if (method === 'POST') {
         // user登录
         if (req.path === '/api/user/login') {
-            // return { msg: '这是登录的接口' }
-            const { username, password } = req.query;
+            const { username, password } = req.body;
             const result = await login(username, password)
             if (result.username) {
                 req.session.username = result.username
@@ -19,11 +18,6 @@ const handleUserRouter = async (req) => {
                 return new ErrorModel('登录失败')
             }
         }
-    }
-    if (req.path === '/api/user/testLogin') {
-        if (req.session.username) {
-            return Promise.resolve(new SuccessModel(req.session))
-        } else return Promise.resolve(new ErrorModel('尚未登录'))
     }
 }
 module.exports = handleUserRouter
